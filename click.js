a3ad8b9bbabd338d91c284903e1c7a11e466db65
MikeL538 <m.lipiak97@gmail.com>
2023-05-05 12:14:33 +0200

    Update

--- click.js
+++ click.js
@@ -1,5 +1,6 @@
 const flipBtn = document.querySelector(".div-button");
 const cardContainer = document.querySelector(".container");
+const vanish = document.querySelectorAll(".vanish");
 
 flipBtn.addEventListener("click", function () {
   if (cardContainer.classList.contains("flip")) {
@@ -10,5 +11,21 @@ flipBtn.addEventListener("click", function () {
     cardContainer.classList.add("flip");
   } else {
     cardContainer.classList.add("flip");
+  }
+
+  if (cardContainer.classList.contains("flip")) {
+    vanish.forEach((element, index) => {
+      element.classList.add("disappear");
+      element.classList.remove("appear");
+    });
+  }
+
+  if (cardContainer.classList.contains("flip-back")) {
+    vanish.forEach((element, index) => {
+      setTimeout(() => {
+        element.classList.remove("disappear");
+        element.classList.add("appear");
+      }, index * 50);
+    });
   }
 });
